dff73c8b5144bfb905c8753ea69b9ac8153cbfe7
Maxime Salomon <maxime@croissant.io>
2019-01-30 13:38:58 +0100

    addEventListener Click on Menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,18 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.getElementsByClassName('menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.getElementsByClassName('menu-button');
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+console.log(menuButton[0]);
+
+menuButton[0].addEventListener('click', function(){
+  menu[0].classList.toggle('menu--open');
+});
